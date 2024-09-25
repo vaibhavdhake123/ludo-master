693d81bd655740b1ad5e57ce852e9254c97cf07d
DHAKE VAIBHAV NITIN <dhakev1@gmail.com>
2024-09-25 10:53:39 +0530

    NEW CODE

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+
+    container : {
+    flex : 1,
+    backgroundColor : 'white',
+    }
+})
+
